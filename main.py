ca5f41c39604522c863c4437c7f9248c7945f91b
PajarKharisma <pajarkharisma@hotmail.com>
2020-09-16 20:00:42 +0700

    init commit

--- main.py
+++ main.py
@@ -15,23 +15,15 @@ def get_solution(dir_solution):
     return results
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-# checkpoint = torch.load('model-850a84c89d914d99a984619ba3fe1338.pth', map_location=device)
-checkpoint = torch.load('smart-farming-model.pth', map_location=device)
-# for sol in checkpoint['solution']:
-#     print(sol)
-solutions = get_solution('solution')
-print(len(solutions))
-for i in range(len(solutions)):
-    print(solutions[i][0])
-    print()
-# checkpoint = {
-#         'title' : checkpoint['title'],
-#         'epoch': checkpoint['epoch'],
-#         'loss' : checkpoint['loss'],
-#         'class_names' : checkpoint['class_names'],
-#         'desc' : checkpoint['desc'],
-#         'solution' : solutions,
-#         'state_dict': checkpoint['state_dict'],
-#         'optimizer': checkpoint['optimizer']
-#     }
-# torch.save(checkpoint, 'smart-farming-model.pth')
+checkpoint = torch.load('model-850a84c89d914d99a984619ba3fe1338.pth', map_location=device)
+checkpoint = {
+        'title' : checkpoint['title'],
+        'epoch': checkpoint['epoch'],
+        'loss' : checkpoint['loss'],
+        'class_names' : checkpoint['class_names'],
+        'desc' : checkpoint['desc'],
+        'solution' : solutions,
+        'state_dict': checkpoint['state_dict'],
+        'optimizer': checkpoint['optimizer']
+    }
+torch.save(checkpoint, 'smart-farming-model.pth')
